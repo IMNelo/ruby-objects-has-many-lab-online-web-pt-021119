beafd373ca92a6eca9bf397ca4b9d7e7bd961746
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-24 07:06:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,11 @@ class Artist
 
   attr_accessor :name, :artist
 
+  @songs = []
+
   def initialize(name, artist)
     @name = name
     @artist = artist
-    @songs = []
   end
 
   def add_song(song)
