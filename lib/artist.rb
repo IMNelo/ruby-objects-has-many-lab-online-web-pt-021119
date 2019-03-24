527293aab7f792e3e3eca2fe0c4c5d0629154687
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-24 06:45:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,15 +7,13 @@ class Artist
     @songs = []
   end
 
-  def songs #returns a collection of all the songs that belong to the artist
-    @songs
-  end
-
-
-
   def add_song_by_name(song)
     @songs << song
     song.artist = self
+  end
+
+  def songs #returns a collection of all the songs that belong to the artist
+    @songs
   end
 
   def song_count
