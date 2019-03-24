8da14bdb086273f119cf24b9f6021d18dc59780b
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-24 06:41:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
 
 
 
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
   end
