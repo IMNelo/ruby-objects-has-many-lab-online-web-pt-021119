4f2cfdb2d459d1b481273f1007fb089238dfd6d0
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-24 06:54:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,9 @@ class Song
 
   attr_accessor :name, :artist
 
-  #@@songs = []  #class variable-> holds instances of every song that's been created
+  def initialize(name, artist)
+    @name = name
+    @artist = artist
+  end
 
-#  def initialize(name, artist)
-
-#    @name = name
-#    @artist = artist
-#  end
-
-#  def #self.songs  #class method-> returns class variable containing song collection
-    #@@songs
-  #end
 end
