fdcf561ecbaa745911aefbb3d9a88d33bd2d49f7
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-24 07:48:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,21 @@
 class Author
 
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post
+    @posts << post
+    post.author = self #Associates each post w/the author. Tells each post that "IT" belongs to the author.
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+  end
+  
 end
